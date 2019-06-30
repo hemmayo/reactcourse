580f8a59c422e912783d4a75a9131d179ea210ef
Emmanuel Popoola <olaayo10@gmail.com>
2019-06-30 20:49:59 +0100

    api image id padding

--- 05_pokedex_project/pokemon/src/Pokecard.js
+++ 05_pokedex_project/pokemon/src/Pokecard.js
@@ -3,11 +3,12 @@ import './Pokecard.css'
 
 // const POKE_API = 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/'
 const POKE_API = 'https://assets.pokemon.com/assets/cms2/img/pokedex/detail/'
+let padToThree = (number) => number <= 999 ? `00${number}`.slice(-3) : number;
 
 export default class Pokecard extends Component {
     render() {
         let props = this.props;
-        let imgSrc = `${POKE_API}${props.id}.png`;
+        let imgSrc = `${POKE_API}${padToThree(props.id)}.png`;
         
         return (
             <div className="Pokecard">
