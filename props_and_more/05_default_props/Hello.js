feb9a13203029c97bd56245cce4021ac0dc7464e
Emmanuel Popoola <olaayo10@gmail.com>
2019-06-30 15:22:13 +0100

    default props

---
+++ props_and_more/05_default_props/Hello.js
@@ -0,0 +1,14 @@
+class Hello extends React.Component {
+	static defaultProps = {
+		from: 'Anonymous'
+	}
+	render() {
+		let bangs = "!".repeat(this.props.bangs);
+		return (
+			<div>
+				<p>Hi {this.props.to} from {this.props.from}{bangs}</p>
+				<img src={this.props.img} />
+			</div>
+		);
+	}
+}
